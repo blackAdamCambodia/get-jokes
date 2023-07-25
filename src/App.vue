1b67f27e0e53148a76bbfcb124f5daaf787dee98
blackAdamCambodia <blackadam2000kh@gmail.com>
2023-07-26 02:19:24 +0700

    how to fetch data

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, reactive} from "vue";
+const jokes= reactive({
+  value : "Chuck Norris can prepare a Vegan dish out of pork, beef, chicken, eggs, milk, and butter."
+});
+
+
+
+</script>
+
+<template>
+  <section class="flex-col  justify-content-center align-items-center ">
+    <p class="font-bold">{{ jokes.value }}</p>
+    <button @click=" getJokes()" class="bg-green-600 p-3 text-white mt-3 " >Get a new Joke</button>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
